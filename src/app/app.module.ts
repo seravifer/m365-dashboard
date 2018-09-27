45493481c0689a07b6ae321f4b6aa6010be64c8b
seravifer <sergiavilafernandez@gmail.com>
2018-09-27 16:32:50 +0200

    Added BLE module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, RouteReuseStrategy, Routes } from '@angular/router';
+import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { BLE } from '@ionic-native/ble/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    BLE,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { BLE } from '@ionic-native/ble/ngx';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  devices: any[];
+
+  constructor(
+    private bluetooth: BLE
+  ) {}
+
+  ngOnInit() {
+    this.bluetooth.scan([], 60).subscribe(res => {
+      this.devices.push(res);
+      console.log(res);
+    }, err => {
+      console.warn('Bluetooth not found!');
+    })
+  }
+
+  onSelectDevice(id: string) {
+    this.bluetooth.connect(id).subscribe(res =>  {
+      console.log('Conected device!');
+    }, err => {
+      console.log('Device connection error!');
+    })
+  }
 
 }
